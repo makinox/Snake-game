b570072092cfc17591b6b032cef60e8459652f08
Jesus Bossa <x@jesusbossa.dev>
2021-01-17 16:05:32 -0500

    [IMPROVEMENT] Helmet memory

--- src/components/helmet/helmet.ts
+++ src/components/helmet/helmet.ts
@@ -1,5 +1,5 @@
 import Icon from '../../assets/images/Logo.svg';
-import Manifest from '../../assets/data/manifest.webmanifest';
+// import Manifest from '../../assets/data/manifest.webmanifest';
 
 export default function Helmet() {
   const faviconElement = document.createElement('link');
